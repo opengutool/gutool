055f1fd7015f59c3c37b72940862a4d9aed25207
gutool <gutool@163.com>
2025-09-29 11:56:32 +0800

    Add macos icns

--- view/src/assembly/bin/gutool.sh
+++ view/src/assembly/bin/gutool.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 BASE_DIR="$(dirname "$0")/.."
+
+# platform ops
+P_OPTS=
+
+case `uname` in
+   Darwin*)
+   P_OPTS="-Xdock:name=Gutool -Xdock:icon=$BASE_DIR/icns/gutool.icns -Dapple.laf.useScreenMenuBar=true -Dapple.eawt.quitStrategy=CLOSE_ALL_WINDOWS"
+   ;;
+esac
+
+
 java -Dswing.aatext=true \
 -XX:+UseZGC \
 -XX:+ZGenerational \
@@ -12,4 +23,5 @@ java -Dswing.aatext=true \
 --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
 --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
 --add-opens java.base/java.lang=ALL-UNNAMED \
--jar "$BASE_DIR/gutool.jar" "$@"
+-jar $P_OPTS \
+"$BASE_DIR/gutool.jar" "$@"
